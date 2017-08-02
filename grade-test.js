abb6fed81bc78a631005330bf789ee4697a71906
cymun111 <cymun111@gmail.com>
2017-08-02 01:57:55 -0700

    avg is now working correctly

--- grade-test.js
+++ grade-test.js
@@ -21,34 +21,46 @@ var x = event.keyCode;
   if (x == 32) {$("#gradeTest").click(function() {
   var valueInput = $("#myInput").val();
   var valueSplit = valueInput.split(" ", 4);
-  firstGrade = valueSplit[0], secondGrade = valueSplit[1], thirdGrade = valueSplit[2], fourthGrade = valueSplit[3];
-  let avg = (+firstGrade + +secondGrade + +thirdGrade + +fourthGrade) / valueSplit.length;
+  firstGrade = parseInt(valueSplit[0]), secondGrade = parseInt(valueSplit[1]), thirdGrade = parseInt(valueSplit[2]), fourthGrade = parseInt(valueSplit[3]);
+  let avg = (firstGrade + secondGrade + thirdGrade + fourthGrade) / valueSplit.length;
+  console.log(avg);
+
   if (A.indexOf(avg) >= 0){
   document.getElementById('answer').innerHTML = "A";}
     if (A.indexOf(avg) >= 0){
     document.getElementById('average').innerHTML = "Your average Grade is" + ' ' + avg;}
-     else if(B.indexOf(avg) >= 0){
+     else if(B.indexOf(avg) >= 0 | B.indexOf(avg) <= 9){
       document.getElementById('answer').innerHTML = "B";}
-        if (B.indexOf(avg) >= 0){
+        if (B.indexOf(avg) >= 0 | B.indexOf(avg) <= 9){
         document.getElementById('average').innerHTML = "Your average Grade is" + ' ' + avg;}
-         else if (C.indexOf(avg) >= 0){
+         else if (C.indexOf(avg) >= 0 | C.indexOf(avg) <= 9){
           document.getElementById('answer').innerHTML = "C";}
-          if (C.indexOf(avg) >= 0){
+          if (C.indexOf(avg) >= 0 | C.indexOf(avg) <= 9){
            document.getElementById('average').innerHTML = "Your average Grade is" + ' ' + avg;}
-            else if (D.indexOf(avg) >= 0){
+            else if (D.indexOf(avg) >= 0 | D.indexOf(avg) <= 9){
              document.getElementById('answer').innerHTML = "D";}
-             if (D.indexOf(avg) >= 0){
+             if (D.indexOf(avg) >= 0 | D.indexOf(avg) <= 9){
              document.getElementById('average').innerHTML = "Your average Grade is" + ' ' + avg;}
-              else if (F.indexOf(avg) >= 0){
+              else if (F.indexOf(avg) <= 9){
                document.getElementById('answer').innerHTML = "F";}
-               if (F.indexOf(avg) >= 0){
+               if (F.indexOf(avg) <= 9){
                document.getElementById('average').innerHTML = "Your average Grade is" + ' ' + avg;}
-});}
-});
-
-$("#myInput").click(function(){
-  document.getElementById("#myInput").value = "";
-
+});}else{
+$("#gradeTest").click(function(){
+    let valueInput = parseInt(document.getElementById("myInput").value);
+      if (A.indexOf(valueInput) >= 0){
+        document.getElementById('answer').innerHTML = "A";}
+          else if(B.indexOf(valueInput) >= 0){
+            document.getElementById('answer').innerHTML = "B";}
+            else if (C.indexOf(valueInput) >= 0){
+              document.getElementById("answer").innerHTML = "C";}
+              else if (D.indexOf(valueInput) >= 0){
+                document.getElementById("answer").innerHTML = "D";}
+                else if (F.indexOf(valueInput) >= 0){
+                  document.getElementById("answer").innerHTML = "F";}
+                  else if(valueInput > 100 || valueInput < 0){
+                  document.getElementById("answer").innerHTML = "Invalid Number";}
+                });}
 });
     /*var valueInput2 = document.getElementById("myInput");
     var array = valueInput2.split(' ', 4),
